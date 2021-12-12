a61b183168bb5fbb906a7368cc286024f6fffb6d
RoxanaRoxana <roxana0804@gmail.com>
2021-12-12 16:23:19 +0100

    .

---
+++ src/js/01-gallery.js
@@ -0,0 +1,41 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+
+console.log(galleryDiv);
+
+galleryItems.forEach(item => {
+  galleryDiv.insertAdjacentHTML(
+    'afterbegin',
+    `
+  <a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.original}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+`,
+  );
+  const link = document.querySelector('.gallery__item');
+
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+  });
+});
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: 'true',
+  captionSelector: 'img',
+  captionType: 'string',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+});
